8782de4257e1801d6a580af1fd52580edbfdfcac
inok6743 <inok6743@gmail.com>
2018-04-17 21:45:29 +0900

    Deploy converted files to our web site after build.

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+./build.py local
+
+if [ "${TRAVIS_BRANCH}" = "master" ]; then
+    DEPLOY_DIR=iitc/release
+else
+    DEPLOY_DIR=iitc/development
+fi
+
+git clone https://github.com/inok6743/enl-tottori-web.git deploy_target
+cp -f ./build/local/*.user.js "./deploy_target/${DEPLOY_DIR}/"
+cp -f ./build/local/plugins/*.user.js "./deploy_target/${DEPLOY_DIR}/"
+
+cd ./deploy_target/
+git config --global user.email 'inok6743@gmail.com'
+git config --global user.name 'Travis CI (Automatic Commit)'
+git add './iitc/*.user.js'
+git commit -m "IITC: Automatic deploy from Travis-CI (#${TRAVIS_BUILD_NUMBER})"
+git push "https://${GH_TOKEN}@github.com/enl-tottori/enl-tottori-web.git" master:master
